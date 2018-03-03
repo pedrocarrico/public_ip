3e20743588f5a888f41baee7fcc6ce4f3ff7f586
Pedro Carriço <pedro.carrico@gmail.com>
2018-03-03 19:14:29 +0000

    Add custom headers to services that block requests

--- lib/public_ip/service/ifconfig_me.rb
+++ lib/public_ip/service/ifconfig_me.rb
@@ -4,6 +4,10 @@ module PublicIp
       def self.uri
         URI('http://ifconfig.me/ip')
       end
+
+      def self.headers
+        { 'User-Agent' => 'curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)' }
+      end
     end
   end
 end
